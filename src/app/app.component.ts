08b6d997641023c4baae146f54a8d058b3374ec8
jmart210 <crusader11jm@yahoo.com>
2017-10-11 15:35:08 -0500

    pratice templates and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,12 @@ import { NgModule } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`
+    h3 {
+      color: dodgerblue;
+    }
+  `]
 })
 
 export class AppComponent {
