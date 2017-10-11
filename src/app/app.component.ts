f0215b91215c4933b5be7c9f3bea100ee026d137
jmart210 <crusader11jm@yahoo.com>
2017-10-11 14:22:52 -0500

    add bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// @NgModule({
+//   imports: [
+//     FormsModule
+//   ],
+// })
+
 export class AppComponent {
-  title = 'app';
+  name = '';
 }
